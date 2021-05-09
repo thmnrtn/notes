a6d8b4608ebae9a9b07b0f073ec7e29dfd1e6174
Thom Norton <thom.norton@ors.org.uk>
2021-05-09 08:02:18 +0100

    Initial setup

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Notes</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <link rel="stylesheet" href="{{ asset(mix('/css/app.css')) }}">
+    <script src="{{ asset(mix('/js/app.js')) }}" defer></script>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <span class="navbar-brand mb-0 h1">My Notes</span>
+    </nav>
+    @inertia
+</body>
+</html>
